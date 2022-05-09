d23e00ada180cde4d51a4ea3c085db37329dc1aa
AlanRayburn <alanrayburn85@gmail.com>
2022-05-09 18:21:56 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ from feedgen.feed import FeedGenerator
 from lxml.etree import CDATA
 
 MD_HEAD = """## Gitblog
-My personal blog using issues and GitHub Actions (随意转载，无需署名)
-[RSS Feed](https://raw.githubusercontent.com/{repo_name}/master/feed.xml)
+My personal blog using issues and GitHub Actions    
+[RSS Feed](https://raw.githubusercontent.com/{repo_name}/main/feed.xml)
 """
 
 BACKUP_DIR = "BACKUP"
